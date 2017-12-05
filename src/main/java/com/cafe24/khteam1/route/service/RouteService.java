da37b412294dd1bd1a0c073de1a1a006c9477803
JAYS\Jay <jwelisabeth@gmail.com>
2017-12-05 18:49:59 +0900

    admin page creat route success

--- src/main/java/com/cafe24/khteam1/route/service/RouteService.java
+++ src/main/java/com/cafe24/khteam1/route/service/RouteService.java
@@ -3,13 +3,25 @@ package com.cafe24.khteam1.route.service;
 import java.util.List;
 import java.util.Map;
 
-
+import javax.servlet.http.HttpServletRequest;
 
 public interface RouteService {
 
-	//회원리스트
+	// 회원리스트
 	List<Map<String, Object>> depRouteList(Map<String, Object> map) throws Exception;
-	
+
 	List<Map<String, Object>> arrRouteList(Map<String, Object> map) throws Exception;
+
+	List<Map<String, Object>> selectRouteList(Map<String, Object> map) throws Exception;
+
+	List<Map<String, Object>> selectRouteDeactList(Map<String, Object> map) throws Exception;
+
+	void insertRoute(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
+	Map<String, Object> selectRouteDetail(Map<String, Object> map) throws Exception;
+
+	void updateRoute(Map<String, Object> map, HttpServletRequest request) throws Exception;
+
+	void deleteRoute(Map<String, Object> map) throws Exception;
 
 }
--- src/main/java/com/cafe24/khteam1/route/service/RouteServiceImpl.java
+++ src/main/java/com/cafe24/khteam1/route/service/RouteServiceImpl.java
@@ -1,9 +1,11 @@
 package com.cafe24.khteam1.route.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
@@ -32,5 +34,42 @@ public class RouteServiceImpl implements RouteService {
 	}
 
 	
+	@Override
+	public List<Map<String, Object>> selectRouteList(Map<String, Object> map) throws Exception {
+	    return routeDAO.selectRouteList(map);
+	}
+	
+	@Override
+	public List<Map<String, Object>> selectRouteDeactList(Map<String, Object> map) throws Exception {
+	    return routeDAO.selectRouteDeactList(map);
+	}
+
+	@Override
+	public void insertRoute(Map<String, Object> map, HttpServletRequest request) throws Exception {
+	       
+		routeDAO.insertRoute(map);
+	}
+	
+	@Override
+	public Map<String, Object> selectRouteDetail(Map<String, Object> map) throws Exception {
+		   
+		Map<String, Object> resultMap = new HashMap<String,Object>();
+		Map<String, Object> tempMap = routeDAO.selectRouteDetail(map);
+		resultMap.put("map", tempMap);
+		        
+		System.out.println(resultMap); 
+		return resultMap;
+	}
+	
+	@Override
+	public void updateRoute(Map<String, Object> map, HttpServletRequest request) throws Exception{
+		routeDAO.updateRoute(map);
+	}
+	
+	@Override
+	public void deleteRoute(Map<String, Object> map) throws Exception {
+		routeDAO.deleteRoute(map);
+	}
+
 
 }
